bf3083b7cdcb81c9ab6ba40c376677e4c644e053
vijayvir singh <apple@qwerty.local>
2017-02-15 10:22:44 +0530

    singleTom

---
+++ Swift/ArchitectureConcepts/ArchitectureConcepts/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ArchitectureConcepts
+//
+//  Created by Apple on 15/02/17.
+//  Copyright © 2017 vijayvirSingh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}

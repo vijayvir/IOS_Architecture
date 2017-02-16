2fddcc0dc5b84f493581850daea904868f13e134
vijayvir singh <apple@qwerty.local>
2017-02-16 12:12:28 +0530

    Swift Sigle ton
    Swift Sigle ton

--- Objectivec/Singletons/Singletons/ViewController.m
+++ Objectivec/Singletons/Singletons/ViewController.m
@@ -37,6 +37,22 @@
     NSLog(@"3 %@",sharedManager33.description);
     
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    self.txtVLogs.text = [NSString stringWithFormat:@"%@    %@ " , sharedManager11.description , sharedManager33.description];
+    
+    
+//    2017-02-15 21:07:53.822 Singletons[15765:319999] <ShareManager: 0x60000001c250>
+    
+//    2017-02-15 21:07:53.823 Singletons[15765:319999] 3 <ShareManager: 0x60800001bfa0>
+    
+//    2017-02-15 21:07:53.823 Singletons[15765:319999] <ShareManager: 0x60000001c250>
+    
+//    2017-02-15 21:07:53.823 Singletons[15765:319999] 3 <ShareManager: 0x60800001bfa0>
+    
+
+    
 }
 
 
--- Swift/ArchitectureConcepts/ArchitectureConcepts/ViewController.swift
+++ Swift/ArchitectureConcepts/ArchitectureConcepts/ViewController.swift
@@ -8,16 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Foundation
 
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        let instance = SimpleSwiftClass.sharedInstance()
+        
+        print(instance.simpleFunc())
+        
+        let instance2 = SimpleSwiftClass.sharedInstance
+        
+        print(instance2.simpleFunc())
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+}
 
+class SimpleSwiftClass
+{
+    
+    private static var shareManager: SimpleSwiftClass =
+        {
+        
+        let shareManager = SimpleSwiftClass()
+        
+        return shareManager
+        
+    }()
+    
+    private init()
+    {
+        
+    }
+    
+    func simpleFunc()
+    {
+        print("HelloWorld")
+    }
+    
+    class var sharedInstance: SimpleSwiftClass
+    {
+        print("var")
+        
+        return shareManager
+    }
+    
+    class func sharedInstance(sting _: String? = nil) -> SimpleSwiftClass
+    {
+        print("func")
+        return shareManager
+    }
+    
 }
